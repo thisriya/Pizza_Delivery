029d9d1e1f08f951a3ae982e0433539e06ea18f1
Riya Singh <singh.riyarajput2003@gmail.com>
2024-09-12 15:07:26 +0530

    made login and register page

--- server.js
+++ server.js
@@ -6,13 +6,28 @@ const expressLayout=require('express-ejs-layouts')
 const path=require('path')
 
 app.use(express.static('public'))
-app.get('/', function(req,res){
-    res.render("home")
-})
 
 app.use(expressLayout)
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
+
+app.get('/', function(req,res){
+    res.render("home")
+})
+
+app.get('/login', function(req,res){
+    res.render("auth/login")
+})
+
+app.get('/register', function(req,res){
+    res.render("auth/register")
+})
+
+
+app.get('/cart',(req,res)=>{
+    res.render('customer/cart')
+})
+
 app.listen(PORT,()=>{
     console.log(`Listening on port ${PORT}`)
 })
